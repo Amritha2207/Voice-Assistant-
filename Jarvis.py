30759e869291525c82912fc3f4cddfaef8d1e528
Amritha <90847784+Amritha2207@users.noreply.github.com>
2023-09-25 23:32:59 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -10,11 +10,9 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-
 
 def take_command():
     try:
@@ -23,13 +21,12 @@ def take_command():
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'Jarvis' in command:
-                command = command.replace('alexa', '')
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
                 print(command)
     except:
         pass
     return command
-
 
 def run_alexa():
     command = take_command()
@@ -42,7 +39,7 @@ def run_alexa():
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
     elif 'who is' in command:
-        person = command.replace('who the heck is', '')
+        person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
@@ -51,12 +48,11 @@ def run_alexa():
     elif 'are you single' in command:
         talk('I am in a relationship with wifi')
     elif 'are you okay' in command:
-        talk('sorry , i have a headache')
+        talk('Sorry, I have a headache')
     elif 'joke' in command:
         talk(pyjokes.get_joke())
     else:
         talk('Please say the command again.')
-
 
 while True:
     run_alexa()
