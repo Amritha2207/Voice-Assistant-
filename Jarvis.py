7ed1fd52c602ce216b1ea9badbd10542f9f7568f
Amritha <90847784+Amritha2207@users.noreply.github.com>
2023-09-26 09:28:59 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,58 +1,85 @@
-import speech_recognition as sr
 import pyttsx3
-import pywhatkit
-import datetime
+import speech_recognition as sr
 import wikipedia
-import pyjokes
+import webbrowser
+import os
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+# init pyttsx
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
 
-def talk(text):
-    engine.say(text)
+engine.setProperty('voice', voices[0].id)  # 1 for female and 0 for male voice
+
+
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
+
 def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
     try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'jarvis' in command:
-                command = command.replace('jarvis', '')
-                print(command)
-    except:
-        pass
-    return command
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print("User said:" + query + "\n")
+    except Exception as e:
+        print(e)
+        speak("I didnt understand")
+        return "None"
+    return query
 
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who is' in command:
-        person = command.replace('who is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'who created you' in command:
-        talk('Amritha created me')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'are you okay' in command:
-        talk('Sorry, I have a headache')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
 
-while True:
-    run_alexa()
+if __name__ == '__main__':
+
+    speak("Jarvis assistance activated ")
+    speak("How can i help you")
+    while True:
+        query = take_command().lower()
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia ...")
+            query = query.replace("wikipedia", '')
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            speak(results)
+        elif 'are you' in query:
+            speak("I am amigo developed by Jaspreet Singh")
+        elif 'open youtube' in query:
+            speak("opening youtube")
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            speak("opening google")
+            webbrowser.open("google.com")
+        elif 'open github' in query:
+            speak("opening github")
+            webbrowser.open("github.com")
+        elif 'open stackoverflow' in query:
+            speak("opening stackoverflow")
+            webbrowser.open("stackoverflow.com")
+        elif 'open spotify' in query:
+            speak("opening spotify")
+            webbrowser.open("spotify.com")
+        elif 'open whatsapp' in query:
+            speak("opening whatsapp")
+            loc = "C:\\Users\\jaspr\\AppData\\Local\\WhatsApp\\WhatsApp.exe"
+            os.startfile(loc)
+        elif 'play music' in query:
+            speak("opening music")
+            webbrowser.open("spotify.com")
+        elif 'play music' in query:
+            speak("opening music")
+            webbrowser.open("spotify.com")
+        elif 'local disk d' in query:
+            speak("opening local disk D")
+            webbrowser.open("D://")
+        elif 'local disk c' in query:
+            speak("opening local disk C")
+            webbrowser.open("C://")
+        elif 'local disk e' in query:
+            speak("opening local disk E")
+            webbrowser.open("E://")
+        elif 'sleep' in query:
+            exit(0)
